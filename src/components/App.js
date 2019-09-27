1d8dab4de103a7a46b4ecdfec8ed7f128471f63e
Szymon Warszawa <szymonwarszawa@gmail.com>
2019-09-27 21:10:31 +0100

    added task removing and modifying methods

--- src/components/App.js
+++ src/components/App.js
@@ -27,10 +27,31 @@ class App extends Component {
 
   deleteTask = id => {
     console.log("delete element " + id);
+    // const tasks = [...this.state.tasks];
+    // const index = tasks.findIndex(task => task.id === id);
+    // tasks.splice(index, 1);
+    // this.setState({
+    //   tasks
+    // });
+    let tasks = [...this.state.tasks];
+    tasks = tasks.filter(task => task.id !== id);
+    this.setState({
+      tasks
+    });
   };
 
   changeTaskStatus = id => {
     console.log("change of element " + id);
+    const tasks = Array.from(this.state.tasks);
+    tasks.forEach(task => {
+      if (task.id === id) {
+        task.active = false;
+        task.finishDate = new Date().getTime();
+      }
+    });
+    this.setState({
+      tasks
+    });
   };
 
   render() {
